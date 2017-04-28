8fb482e1b94afe4bb541917c3e78bf11398f9a0d
Havorax <havorax@gmail.com>
2017-04-27 20:11:22 -0700

    b wazv heer

--- Reach/app/src/test/java/com/davidbigd/reach/ExampleUnitTest.java
+++ Reach/app/src/test/java/com/davidbigd/reach/ExampleUnitTest.java
@@ -9,9 +9,11 @@ import static org.junit.Assert.*;
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
+// b
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
     }
-}
+} 
+// b
